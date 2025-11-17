18f1608bc41e41e0072ad5a9b16f17c1d6a12d23
tfermoso <tomas.fermoso@gmail.com>
2025-11-17 17:25:53 +0100

    detectamos movimiento raqueta

--- js/canvas.js
+++ js/canvas.js
@@ -5,13 +5,26 @@ let y = 50//canvas.height / 2; // centrada verticalmente
 let speedx = 3;
 let speedy = 1;             // velocidad hacia la izquierda
 let radius = 20;           // tamaño de la bola
+let raquetax = 0
+let raquetay = canvas.height - 10;
+let raquetaWidth = 40;
+document.addEventListener("keydown", moverRaqueta);
 
+function moverRaqueta(e) {
+    if(e.key=="ArrowRight"){
+        console.log("derecha");
+    }
+}
 
 function draw() {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fillStyle = "red";
     ctx.fill();
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(raquetax, raquetay, raquetaWidth, 10);
+
     ctx.closePath();
 }
 
