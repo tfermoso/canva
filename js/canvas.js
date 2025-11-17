e7cf8f7aaf37232e5e6df798d0363722dc163d52
tfermoso <tomas.fermoso@gmail.com>
2025-11-17 17:01:24 +0100

    nos movemos en y

--- js/canvas.js
+++ js/canvas.js
@@ -2,31 +2,38 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 let x = 50//canvas.width + 20; // empieza fuera del canvas (a la derecha)
 let y = 50//canvas.height / 2; // centrada verticalmente
-let speedx = 3;             // velocidad hacia la izquierda
+let speedx = 3;
+let speedy = 1;             // velocidad hacia la izquierda
 let radius = 20;           // tamaño de la bola
 
 
 function draw() {
     ctx.beginPath();
-    ctx.arc(x, y, radius,0 ,Math.PI * 2);
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
 }
 
-function update(){
-    x+=speedx;
-      if (x + radius > canvas.width || x - radius < 0) {
-    speedx *= -1;
-  }
+function update() {
+    x += speedx;
+    if (x + radius > canvas.width || x - radius < 0) {
+        speedx *= -1;
+    }
+    y += speedy;
+    if (y + radius > canvas.height || y - radius < 0) {
+        speedy *= -1;
+    }
+
+
 }
 function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  update();
-  draw();
+    update();
+    draw();
 
-  requestAnimationFrame(loop);
+    requestAnimationFrame(loop);
 }
 
 
