98b62cc5f58f48a5c42d801f191d23eeac71ec96
tfermoso <tomas.fermoso@gmail.com>
2025-11-17 16:30:36 +0100

    dibujo bola

---
+++ js/canvas.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+let x = canvas.width + 20; // empieza fuera del canvas (a la derecha)
+let y = canvas.height / 2; // centrada verticalmente
+let speed = 3;             // velocidad hacia la izquierda
+let radius = 20;           // tamaño de la bola
+
+
+function draw() {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+}
+
